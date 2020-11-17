006fca6240e84bd1caf5e488d81405bddac22981
chinnamlakshmibhavani <46337222+chinnamlakshmibhavani@users.noreply.github.com>
2020-11-17 17:08:49 +0530

    added code using Annotations configurations and Bean to achieve dependency injection

---
+++ SpringAnnotation/src/main/java/com/lucky/SpringAnnotation/AppConfig.java
@@ -0,0 +1,12 @@
+package com.lucky.SpringAnnotation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class AppConfig
+{
+   @Bean
+   public Samsung getPhone()
+   {
+	    return new Samsung();
+   }
+}
---
+++ SpringAnnotation/src/main/java/com/lucky/SpringAnnotation/Samsung.java
@@ -0,0 +1,9 @@
+package com.lucky.SpringAnnotation;
+
+public class Samsung 
+{
+   public void config()
+   {
+	   System.out.println("Octa Core,4 gb RAM,12MP camera");
+   }
+}
