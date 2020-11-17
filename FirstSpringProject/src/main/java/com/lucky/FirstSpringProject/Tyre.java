ad207d1ef4c02a44e44d91c33a1c817c5cc42de5
chinnamlakshmibhavani <46337222+chinnamlakshmibhavani@users.noreply.github.com>
2020-11-17 18:10:12 +0530

    added constructor for achieving dependency injection

--- FirstSpringProject/src/main/java/com/lucky/FirstSpringProject/Tyre.java
+++ FirstSpringProject/src/main/java/com/lucky/FirstSpringProject/Tyre.java
@@ -3,6 +3,12 @@ package com.lucky.FirstSpringProject;
 public class Tyre 
 {
     private String brand;
+    
+
+	public Tyre(String brand) {
+		super();
+		this.brand = brand;
+	}
 
 	public String getBrand() {
 		return brand;
