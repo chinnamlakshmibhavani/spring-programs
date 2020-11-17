1503d17999371c18a696cc5b78b3148b8248e4e6
chinnamlakshmibhavani <46337222+chinnamlakshmibhavani@users.noreply.github.com>
2020-11-17 17:25:09 +0530

    Add files via upload
    added code for using annotation configuration and bean to achieve dependency injection

---
+++ FirstSpringProject/src/main/java/com/lucky/FirstSpringProject/Bike.java
@@ -0,0 +1,13 @@
+package com.lucky.FirstSpringProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bike implements Vehicle
+{
+	public void drive()
+	{
+	   System.out.println("bike rides");
+	}
+
+}
---
+++ FirstSpringProject/src/main/java/com/lucky/FirstSpringProject/Car.java
@@ -0,0 +1,13 @@
+package com.lucky.FirstSpringProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements Vehicle
+{
+	public void drive()
+	{
+		System.out.println("its working");
+	}
+
+}
